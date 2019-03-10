e01328eb3a95b71e300d2ad75acd3c3c146a94c9
Jay Lee <lyj911111@naver.com>
2019-03-10 11:32:21 +0700

    IP주소값 string을 네트워크 빅엔디안 32비트 정수로 변환

---
+++ Linux/Big_little_Endian_Test_for_CPU/FuncTest_inet_addr.c
@@ -0,0 +1,34 @@
+/*
+	함수 형태
+	in_addr_t  inet_addr  (const char * string);
+	-> 성공시 빅엔디안 변환 32비트 반환,
+	   실패시 INADDR_NONE 반환.
+	   
+	예제, IP주소값을 String으로 전달했을때, 빅엔디안으로 변환된 32비트 값을 받아보기 위함.
+	
+*/
+
+#include <stdio.h>
+#include <arpa/inet.h>
+
+int main(int argc, char * argv[])
+{
+	
+	char * addr1 = "12.34.56.255";	// 1 byte의 최대 크기는 0~255
+	char * addr2 = "12.34.56.256";	// 범위를 벗어났으므로 오류 발생해야 함.
+	
+	unsigned long conv_addr = inet_addr(addr1);
+	if (conv_addr == INADDR_NONE)
+		printf("Error ocrrured");
+	else
+		printf("Network ordered integer addr : %#lx \n", conv_addr);
+	
+	conv_addr = inet_addr(addr2);
+	if (conv_addr == INADDR_NONE)
+		printf("Error ocrrured");
+	else
+		printf("Network ordered integer addr : %#lx \n", conv_addr);
+	
+
+	return 0;
+}
